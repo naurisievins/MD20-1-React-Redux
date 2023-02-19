f383a7e1aeac83a16729383210007435d598ec4f
naurisievins <nauris.ievins@gmail.com>
2023-02-19 22:46:38 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Animals from './components/Animals/Animals'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Animals />
     </div>
   )
 }
